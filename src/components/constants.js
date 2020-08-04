5bb9622ce00369b9cabfcfc469a79220785a34cf
Priyanshu Tyagi <priyanshutyagi@Priyanshus-MacBook-Air.local>
2020-08-04 18:23:05 +0530

    Added the constant declarations

---
+++ src/components/constants.js
@@ -0,0 +1,19 @@
+export const URL = "http://skunkworks.ignitesol.com:8000";
+
+export const Genres = [
+    "Fiction",
+    "Drama",
+    "Humor",
+    "Politics",
+    "History",
+    "Adventure"
+  ];
+  
+  export const BookExtension = [
+    "text/html",
+    "application/pdf",
+    "text/plain; charset=utf-8:"
+  ];
+  
+  export default { URL, Genres, BookExtension };
+  
